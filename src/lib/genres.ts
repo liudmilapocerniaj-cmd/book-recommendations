d0fecba0d3ae43a1c5e78adc431f5a493ad649f3
Liudmila <liudmilapocerniaj@gmail.com>
2026-09-10 23:15:14 +0300

    Expand genre options

--- src/lib/genres.ts
+++ src/lib/genres.ts
@@ -1,7 +1,9 @@
 export const genres = [
   "Grožinė literatūra", "Fantastika", "Mokslinė fantastika", "Detektyvai",
-  "Trileriai", "Romantika", "Istorinė literatūra", "Psichologija", "Saviugda",
-  "Verslas", "Biografijos", "Vaikų literatūra", "Kita",
+  "Trileriai", "Romantika", "Istorinė literatūra", "Siaubo literatūra",
+  "Jaunimo literatūra", "Vaikų literatūra", "Humoras", "Poezija",
+  "Kelionių literatūra", "Psichologija", "Saviugda", "Verslas",
+  "Biografijos", "Esė", "Kita",
 ];
 
 export function decodeGenre(value: string): string {
